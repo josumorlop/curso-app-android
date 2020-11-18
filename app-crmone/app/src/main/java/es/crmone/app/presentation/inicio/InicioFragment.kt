e835cf1d57783666d749abbd9e7ea4df400d5cdc
Ricardo <ricardo.champa@gmail.com>
2020-11-18 21:41:06 +0100

    Tabs

--- app-crmone/app/src/main/java/es/crmone/app/presentation/inicio/InicioFragment.kt
+++ app-crmone/app/src/main/java/es/crmone/app/presentation/inicio/InicioFragment.kt
@@ -1,18 +1,16 @@
-package es.crmone.app.presentation.home
+package es.crmone.app.presentation.inicio
 
 import android.os.Bundle
 import android.view.View
 import es.crmone.app.R
 import es.crmone.app.common.BaseFragment
-import es.crmone.app.databinding.FragmentHomeBinding
+import es.crmone.app.databinding.FragmentInicioBinding
 
-
-class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+class InicioFragment : BaseFragment<FragmentInicioBinding>(R.layout.fragment_inicio) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = FragmentHomeBinding.bind(view)
+        _binding = FragmentInicioBinding.bind(view)
 
     }
-
 }
