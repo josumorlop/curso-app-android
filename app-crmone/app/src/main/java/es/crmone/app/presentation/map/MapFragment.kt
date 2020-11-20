b59f05bac5c7d0de1744800236e0197d551590c1
Josué Morillo <josumorlop@gmail.com>
2020-11-20 21:14:19 +0100

    mas cosicas

--- app-crmone/app/src/main/java/es/crmone/app/presentation/map/MapFragment.kt
+++ app-crmone/app/src/main/java/es/crmone/app/presentation/map/MapFragment.kt
@@ -2,17 +2,31 @@ package es.crmone.app.presentation.map
 
 import android.os.Bundle
 import android.view.View
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import es.crmone.app.R
 import es.crmone.app.common.BaseFragment
 import es.crmone.app.databinding.FragmentMapBinding
 
 
-class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map) {
+class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map), OnMapReadyCallback {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         _binding = FragmentMapBinding.bind(view)
+
+
+        val mapFragment =  childFragmentManager.findFragmentById(R.id.fMap) as SupportMapFragment
+
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+
     }
 
 }
+
+
